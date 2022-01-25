eac72c976c46516eb2ef2239ed573df1162304de
Kamrul Hasan <you@example.com>
2022-01-25 23:52:29 +0600

    JavaScript Keyword, Variable name naming convention and best practice

---
+++ variable-name.js
@@ -0,0 +1,32 @@
+var name = 'Programming Hero';
+
+// reserved keyword not allowed.
+var false = 15;
+var delete = 'delete';
+
+
+// name should be one word.
+var hero name = 'Jhankar Mahbub';
+
+// Qoute is not allowed for the variable name.
+var "heroin" = "Karina";
+
+// dash is not allowed to declare variable name.
+var my-office-address = 'California, USA';
+
+// underscore is allowed but not recomended.
+var my_office_address = 'California, USA';
+
+// Used Camel case to declare variable name, highly recomended.
+var myOfficeAddress = 'California, USA';
+
+var PIE = 3.14;
+
+// first letter should not a number to declare a variable name.
+var 99price = 25;
+
+// it is allowed to declare varable name.
+var price99 = 25;
+
+// used meaningful variable name.
+var user nameHomeAddress = 'Dhaka';
