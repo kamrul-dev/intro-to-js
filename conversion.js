bd91ed1c2ca554dac64eb9934ed35409b9360eb8
Kamrul Hasan <you@example.com>
2022-01-26 00:51:38 +0600

    (advanced) String Concatenation, Integer float parseInt parseFloat type conversion

---
+++ conversion.js
@@ -0,0 +1,40 @@
+var firstName = 'Jennie';
+var lastName = 'Kim';
+
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+
+var first = 'Ariana';
+var last = 'Grande';
+var name = first + ' ' + last;
+// console.log(name);
+
+// var onionPrice = '42';
+// var eggPrice = '41';
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+var onionPrice = '42';
+var eggPrice = '41';
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+// integer
+var sunglass = 3;
+
+//float
+var price = 99.99;
+
+// string/float to integer number
+// var onionPrice = '42';
+// var eggPrice = '41';
+// var onionPriceNumber =  parseInt(onionPrice)
+// console.log(onionPriceNumber);
+
+var onionPrice = '42.50';
+var eggPrice = '41.33';
+var onionPriceNumber =  parseInt(onionPrice)
+var eggPriceNumber =  parseFloat(eggPrice)
+// console.log(onionPriceNumber);
+console.log(eggPriceNumber + onionPriceNumber);
