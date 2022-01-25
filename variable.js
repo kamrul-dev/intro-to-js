3376c2758fe9f156539860989376f6c36701e8c3
Kamrul Hasan <you@example.com>
2022-01-25 23:16:15 +0600

    Variable type, Numeric, String, Boolean

--- variable.js
+++ variable.js
@@ -1,11 +1,19 @@
 var price = 27;
 var age = 24;
 var temperature = 34;
-var section = A;
 var Id = 18103012;
-var firstName = Kamrul;
-var lastName = Hasan;
+var firstName = "Kamrul";
+var lastName = "Hasan";
+var fullName = 'Kamrul Hasan';
+var section = "A";
 var semester = 7;
-var varsity = IUBAT;
-var address = Dhaka;
-var country = Bangladesh;
+var varsity = "IUBAT";
+var address = "Dhaka";
+var country = "Bangladesh";
+var country = 'Bangladesh';
+var secondAddres = "Bashundhara, Dhaka, Bangladesh";
+var secondAddres = 'Bashundhara, Dhaka, Bangladesh';
+
+
+var pass = true;
+var subscribed = false;
